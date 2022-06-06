62494d3035b4bd94e42797c3a74bcda8fb1a2a73
PROGRAMMERHAO <823549721@qq.com>
2022-06-06 14:10:59 +0800

    my changes

--- src/indexbackup.js
+++
@@ -1,8 +0,0 @@
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <ProvideAuth>
-      <App />
-    </ProvideAuth>
-  </React.StrictMode>
-);
